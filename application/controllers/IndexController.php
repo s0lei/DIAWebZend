39a5676854865722cfed692fdf93a3aadd567eb8
sian <sian@P755>
2012-10-29 02:01:54 -0600

    8th commit

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,31 @@
 <?php
 
-class IndexController extends Zend_Controller_Action {
+class IndexController extends Zend_Controller_Action
+{
 
-    public function init() {
+    public function init()
+    {
         /* Initialize action controller here */
     }
 
-    public function indexAction() {
+    public function indexAction()
+    {
         // action body
     }
 
-    public function populatearrivaltableAction() {
+    public function populatearrivaltableAction()
+    {
         $arrivalTable = new Application_Model_DbTable_Arrivalflightschedule();
         $arrivalTable->populateArrivalTable();
     }
+
+    public function arrivalupdatingajaxAction()
+    {
+        // action body
+    }
+
+
 }
+
+
 
