da914049cfb97b3166479a701598119a05de25a2
sian <sian@Vostro-230>
2012-11-02 17:16:14 -0600

    11th commit

--- application/forms/Flightsearch.php
+++ application/forms/Flightsearch.php
@@ -6,22 +6,55 @@ class Application_Form_Flightsearch extends Zend_Form {
         $this->setName('album');
         $id = new Zend_Form_Element_Hidden('id');
         $id->addFilter('Int');
+        
         $artist = new Zend_Form_Element_Text('artist');
         $artist->setLabel('Artist')
                 ->setRequired(true)
                 ->addFilter('StripTags')
                 ->addFilter('StringTrim')
                 ->addValidator('NotEmpty');
+        
         $title = new Zend_Form_Element_Text('title');
         $title->setLabel('Title')
                 ->setRequired(true)
                 ->addFilter('StripTags')
                 ->addFilter('StringTrim')
                 ->addValidator('NotEmpty');
+        
         $submit = new Zend_Form_Element_Submit('submit');
         $submit->setAttrib('id', 'submitbutton');
 
         $this->addElements(array($id, $artist, $title, $submit));
+
+        $this->clearDecorators();
+        $this->addDecorator('FormElements')
+                //->addDecorator('HtmlTag', array('tag' => '<ul>'))
+                ->addDecorator('Form');
+
+        $this->setElementDecorators(array(
+            array('ViewHelper'),
+            array('Errors'),
+            array('Description'),
+            array('Label', array('separator' => ' ')),
+            //array('HtmlTag', array('tag' => 'li', 'class' => 'element-group')),
+        ));
+        
+        $artist->setDecorators(array(
+            array('ViewHelper'),
+            array('Errors'),
+            array('Description'),
+            array('Label', array('separator' => ' ')),
+            //array('HtmlTag', array('tag' => 'li', 'class' => 'leis')),
+        ));
+        
+        // buttons do not need labels
+
+        $submit->setDecorators(array(
+            array('ViewHelper'),
+            array('Description'),
+            //array('HtmlTag', array('tag' => 'li', 'class' => 'submit-group')),
+        ));
     }
+
 }
 
