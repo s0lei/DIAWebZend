715a14c261e4bc15532c828334f81c4e3a515b68
sian <sian@P755>
2012-11-05 08:16:11 -0700

    12th commit

--- application/forms/Flightsearch.php
+++ application/forms/Flightsearch.php
@@ -6,25 +6,35 @@ class Application_Form_Flightsearch extends Zend_Form {
         $this->setName('album');
         $id = new Zend_Form_Element_Hidden('id');
         $id->addFilter('Int');
-        
+
+        $arrangeOrder = new Zend_Form_Element_Select('arrangeOrder');
+        $arrangeOrder->setLabel('Order')
+                ->setMultiOptions(array('airline' => 'Airline', 
+                                        'flightNumber' => 'Flight Number',
+                                        'cityState' => 'City & State', 
+                                        'dateTime' => 'Date & Time', 
+                                        'status' => 'Status'))
+                ->setRequired(true)->addValidator('NotEmpty', true);
+
+
         $artist = new Zend_Form_Element_Text('artist');
         $artist->setLabel('Artist')
                 ->setRequired(true)
                 ->addFilter('StripTags')
                 ->addFilter('StringTrim')
                 ->addValidator('NotEmpty');
-        
+
         $title = new Zend_Form_Element_Text('title');
         $title->setLabel('Title')
                 ->setRequired(true)
                 ->addFilter('StripTags')
                 ->addFilter('StringTrim')
                 ->addValidator('NotEmpty');
-        
+
         $submit = new Zend_Form_Element_Submit('submit');
         $submit->setAttrib('id', 'submitbutton');
 
-        $this->addElements(array($id, $artist, $title, $submit));
+        $this->addElements(array($id, $arrangeOrder, $artist, $title, $submit));
 
         $this->clearDecorators();
         $this->addDecorator('FormElements')
@@ -36,23 +46,23 @@ class Application_Form_Flightsearch extends Zend_Form {
             array('Errors'),
             array('Description'),
             array('Label', array('separator' => ' ')),
-            //array('HtmlTag', array('tag' => 'li', 'class' => 'element-group')),
+                //array('HtmlTag', array('tag' => 'li', 'class' => 'element-group')),
         ));
-        
+
         $artist->setDecorators(array(
             array('ViewHelper'),
             array('Errors'),
             array('Description'),
             array('Label', array('separator' => ' ')),
-            //array('HtmlTag', array('tag' => 'li', 'class' => 'leis')),
+                //array('HtmlTag', array('tag' => 'li', 'class' => 'leis')),
         ));
-        
+
         // buttons do not need labels
 
         $submit->setDecorators(array(
             array('ViewHelper'),
             array('Description'),
-            //array('HtmlTag', array('tag' => 'li', 'class' => 'submit-group')),
+                //array('HtmlTag', array('tag' => 'li', 'class' => 'submit-group')),
         ));
     }
 
